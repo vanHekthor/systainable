95b6192dd23c7fcc6aacb02c83eeefe2308cb50b
Paul Hans Glass <pg98kona@studserv.uni-leipzig.de>
2020-11-27 19:24:34 +0100

    Setup CI and Project Structure

---
+++ backend/src/main/java/org/swtp2015/gcbackend/GreenConfiguratorBackendApplication.java
@@ -0,0 +1,13 @@
+package org.swtp2015.gcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenConfiguratorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenConfiguratorBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/swtp2015/gcbackend/GreenConfiguratorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swtp2015.gcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenConfiguratorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
