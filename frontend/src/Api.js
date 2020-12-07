c589a35a6b081bb3242752518f37b1aae653904b
Quoc Duong Bui <qb19zexe@studserv.uni-leipzig.de>
2020-12-07 13:17:47 +0100

    Added button and request to backend as getFeature-Method in App.vue.

---
+++ frontend/src/Api.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+const SERVER_URL = 'http://localhost:8080';
+
+const instance = axios.create({
+    baseURL: SERVER_URL
+});
+
+export default {
+    // GET
+    // example functions
+    getFeatures:
+        function getFeatures() {
+            instance.get('featuremodels/example/featuremodel1')
+                .then((response) => {
+                    console.log(response.data);
+                    console.log(response.status);
+                    console.log(response.statusText);
+                    console.log(response.headers);
+                    console.log(response.config);
+                })
+        },
+    getProperties:
+        function getProperties(config) {
+            instance.get('configexample', config)
+                .then((response) => {
+                    console.log(response.data);
+                    console.log(response.status);
+                    console.log(response.statusText);
+                    console.log(response.headers);
+                    console.log(response.config);
+                });
+        }
+
+}
