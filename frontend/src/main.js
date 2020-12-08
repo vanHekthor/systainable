301090af01a903cfe206f6c791136fbe17dd066b
Quoc Duong Bui <qb19zexe@studserv.uni-leipzig.de>
2020-12-08 01:26:30 +0100

    used UI Framework Primevue for displaying a configuration table by loading a given feature set. Changing configuration name and turning features on/off is possible.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'primevue/resources/themes/bootstrap4-light-purple/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css';
 
-createApp(App).mount('#app')
+
+const app = createApp(App);
+app.mount('#app')
