fec43175cf57cb4c03956ad68e0a1e0a8c20516d
Quoc Duong Bui <bui.qcd@gmail.com>
2020-12-08 12:32:12 +0100

    Example property values are displayed. Example charts are rendered when submit is clicked.

---
+++ frontend/src/components/ChartArea.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="p-grid">
+    <div class="p-col">
+      <div class="p-shadow-1 p-p-3">
+        <Button type="button" label="Energy: 123" class="p-button-help p-d-block p-mx-auto" />
+      </div>
+    </div>
+    <div class="p-col">
+      <div class="p-shadow-1 p-p-3">
+        <Button type="button" label="CPU: 231" class="p-button-info p-d-block p-mx-auto" />
+      </div>
+    </div>
+    <div class="p-col">
+      <div class="p-shadow-1 p-p-3">
+            <Button type="button" label="Time: 321" class="p-button-danger p-d-block p-mx-auto" />
+      </div>
+    </div>
+  </div>
+  <div class="p-grid">
+    <div class="p-col-12 p-md-6 p-lg-6">
+      <Card>
+        <template #content>
+          <Chart type="radar" :data="chartData" />
+        </template>
+      </Card>
+    </div>
+    <div class="card p-col-12 p-md-6 p-lg-6">
+      <Card>
+        <template #content>
+          <Chart type="bar" :data="basicData" />
+        </template>
+      </Card>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from 'primevue/card';
+import Chart from 'primevue/chart';
+import Button from 'primevue/button';
+
+
+export default {
+  name: "ChartArea",
+  components: {
+    Card,
+    Chart,
+    Button
+  },
+  data() {
+    return {
+      basicData: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'My First dataset',
+            backgroundColor: '#42A5F5',
+            data: [65, 59, 80, 81, 56, 55, 40]
+          },
+          {
+            label: 'My Second dataset',
+            backgroundColor: '#FFA726',
+            data: [28, 48, 40, 19, 86, 27, 90]
+          }
+        ]
+      },
+      chartData: {
+        labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+        datasets: [
+          {
+            label: 'My First dataset',
+            backgroundColor: 'rgba(179,181,198,0.2)',
+            borderColor: 'rgba(179,181,198,1)',
+            pointBackgroundColor: 'rgba(179,181,198,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(179,181,198,1)',
+            data: [65, 59, 90, 81, 56, 55, 40]
+          },
+          {
+            label: 'My Second dataset',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            pointBackgroundColor: 'rgba(255,99,132,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(255,99,132,1)',
+            data: [28, 48, 40, 19, 96, 27, 100]
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'primevue/resources/themes/bootstrap4-light-purple/theme.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css';
