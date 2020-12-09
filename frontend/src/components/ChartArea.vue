0a9082b69d9ec7127b5d37c4eaafd7f38758af5c
Quoc Duong Bui <qb19zexe@studserv.uni-leipzig.de>
2020-12-10 00:07:16 +0100

    Layout changes: dropdown, buttons, distance between button, chart colors. Submit button disabled if no configs in table.

--- frontend/src/components/ChartArea.vue
+++ frontend/src/components/ChartArea.vue
@@ -1,35 +1,39 @@
 <template>
-  <div class="p-grid">
-    <div class="p-col">
-      <div class="p-shadow-1 p-p-3">
-        <Button type="button" label="Energy: 123" class="p-button-help p-d-block p-mx-auto" />
+  <div class="p-mt-4">
+    <div class="p-d-flex p-jc-center">
+      <div class="p-p-3">
+        <Button type="button" label="Energy" class="p-button-help p-d-block p-mx-auto" />
+      </div>
+      <div class="p-p-3">
+        <Button type="button" label="CPU" class="p-button-info p-d-block p-mx-auto" />
+      </div>
+      <div class="p-p-3">
+        <Button type="button" label="Time" class="p-button-danger p-d-block p-mx-auto" />
       </div>
     </div>
-    <div class="p-col">
-      <div class="p-shadow-1 p-p-3">
-        <Button type="button" label="CPU: 231" class="p-button-info p-d-block p-mx-auto" />
+
+    <div class="p-grid">
+      <div class="card p-col-12 p-md-6 p-lg-4">
+        <Card>
+          <template #content>
+            <Chart type="bar" :data="energyData" :options="options"/>
+          </template>
+        </Card>
       </div>
-    </div>
-    <div class="p-col">
-      <div class="p-shadow-1 p-p-3">
-            <Button type="button" label="Time: 321" class="p-button-danger p-d-block p-mx-auto" />
+      <div class="card p-col-12 p-md-6 p-lg-4">
+        <Card>
+          <template #content>
+            <Chart type="bar" :data="cpuData" :options="options"/>
+          </template>
+        </Card>
       </div>
-    </div>
-  </div>
-  <div class="p-grid">
-    <div class="p-col-12 p-md-6 p-lg-6">
-      <Card>
-        <template #content>
-          <Chart type="radar" :data="chartData" />
-        </template>
-      </Card>
-    </div>
-    <div class="card p-col-12 p-md-6 p-lg-6">
-      <Card>
-        <template #content>
-          <Chart type="bar" :data="basicData" />
-        </template>
-      </Card>
+      <div class="card p-col-12 p-md-6 p-lg-4">
+        <Card>
+          <template #content>
+            <Chart type="bar" :data="timeData" :options="options"/>
+          </template>
+        </Card>
+      </div>
     </div>
   </div>
 </template>
@@ -49,19 +53,34 @@ export default {
   },
   data() {
     return {
-      basicData: {
-        labels: ['Energy', 'CPU', 'Time'],
+      energyData: {
+        labels: ['config 1', 'config 2'],
         datasets: [
           {
-            label: 'My First dataset',
-            backgroundColor: '#42A5F5',
-            data: [65, 59, 80]
+            label: 'Energy',
+            backgroundColor: '#9C27B0',
+            data: [2345, 1245]
           },
+        ]
+      },
+      cpuData: {
+        labels: ['config 1', 'config 2'],
+        datasets: [
           {
-            label: 'My Second dataset',
-            backgroundColor: '#FFA726',
-            data: [0.5, 89.3, 5]
-          }
+            label: 'CPU',
+            backgroundColor: '#0288D1',
+            data: [321, 243]
+          },
+        ]
+      },
+      timeData: {
+        labels: ['config 1', 'config 2'],
+        datasets: [
+          {
+            label: 'Time',
+            backgroundColor: '#D32F2F',
+            data: [401, 506]
+          },
         ]
       },
       chartData: {
@@ -88,6 +107,19 @@ export default {
             data: [0.3, 0.63, 0.4]
           }
         ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: true,
+        hoverMode: 'index',
+        stacked: false,
+        scales: {
+          yAxes: [{
+            ticks: {
+              min: 0
+            }
+          }]
+        }
       }
     }
   }
