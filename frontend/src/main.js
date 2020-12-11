68d883b929d164000d8c3e26a13df7f5304d675d
Quoc Duong Bui <bui.qcd@gmail.com>
2020-12-11 01:32:24 +0100

    added missing comma in package.json

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
@@ -7,5 +8,6 @@ import 'primeflex/primeflex.css';
 
 
 const app = createApp(App);
+app.use(PrimeVue);
 app.config.globalProperties.$primevue = {ripple: true};
 app.mount('#app');
