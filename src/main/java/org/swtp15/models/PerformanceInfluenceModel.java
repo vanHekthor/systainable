a7dd169f35383fe2329b9456e5aca3e67b32f705
Torben Levin <tl96vyhu@studserv.uni-leipzig.de>
2020-12-20 16:28:17 +0100

    evaluation of feature configuration #23

---
+++ src/main/java/org/swtp15/models/PerformanceInfluenceModel.java
@@ -0,0 +1,49 @@
+package org.swtp15.models;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class PerformanceInfluenceModel {
+    @Getter
+    private Set<Property> properties;
+
+    @Getter
+    private Set<FeatureInfluence> featureInfluences;
+
+    /**
+     * The Constructor.
+     * @param properties The properties of the Model.
+     * @param featureInfluences The featureInfluences of the Model.
+     */
+    public PerformanceInfluenceModel(Set<Property> properties, Set<FeatureInfluence> featureInfluences){
+        this.properties = properties;
+        this.featureInfluences = featureInfluences;
+    }
+
+    /**
+     * Evaluates the property values for a list of active features.
+     * Iterates over the featureInfluences and adds their value if their set of activeFeatures
+     * is contained in the given set of activeFeatures.
+     * @param activeFeatures Set of activeFeatures.
+     * @return The evaluated property values as a map.
+     */
+    public Map<Property, Double> evaluateConfiguration(Set<String> activeFeatures){
+        Map<Property, Double> evaluation = new HashMap<>();
+        for(var property : properties) {
+            evaluation.put(property, 0.0);
+        }
+        var relatedInfluences = featureInfluences.stream().parallel()
+            .filter(x -> activeFeatures.containsAll(x.getActiveFeatures().stream().map(Feature::getName)
+                .collect(Collectors.toSet()))).collect(Collectors.toSet());
+        for(var featureInfluence : relatedInfluences){
+            for(Map.Entry<Property, Double> entry: featureInfluence.getPropertyInfluence().entrySet()){
+                evaluation.put(entry.getKey(), evaluation.get(entry.getKey()) + entry.getValue());
+            }
+        }
+        return evaluation;
+    }
+}
---
+++ src/test/java/org/swtp15/configurationTests/EvaluationTests.java
@@ -0,0 +1,52 @@
+package org.swtp15.configurationTests;
+
+import org.junit.jupiter.api.Test;
+import org.swtp15.models.*;
+import org.swtp15.parser.FeatureModelParser;
+import org.swtp15.parser.PerformanceModelParser;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EvaluationTests {
+
+    private final FeatureSystem getExampleFeatureSystem(){
+        FeatureModel fm = FeatureModelParser.parseModel("src/test/testFiles/dimacs/CorrectTest.dimacs");
+        PerformanceInfluenceModel pm = PerformanceModelParser.parseModel("src/test/testFiles/csv/CorrectTest.csv", fm.getFeatures());
+        return new FeatureSystem("test", fm, pm);
+    }
+
+    private final FeatureConfiguration getExampleFeatureConfiguration1(){
+        Set<String> activeFeatures = new HashSet<>();
+        activeFeatures.add("feature1");
+        return new FeatureConfiguration("test", activeFeatures);
+    }
+
+    private final FeatureConfiguration getExampleFeatureConfiguration2(){
+        Set<String> activeFeatures = new HashSet<>();
+        activeFeatures.add("feature1");
+        activeFeatures.add("feature2");
+        return new FeatureConfiguration("test", activeFeatures);
+    }
+    @Test
+    void evaluateConfiguration1(){
+        FeatureSystem featureSystem = getExampleFeatureSystem();
+        var result = featureSystem.evaluateFeatureConfiguration(getExampleFeatureConfiguration1());
+        assertEquals(result.size(), 3);
+        assertEquals(result.get(new Property("property_1", null, false)), 0.3, 0.00001);
+        assertEquals(result.get(new Property("property2", null, false)), 0.4, 0.00001);
+        assertEquals(result.get(new Property("property3", null, false)), 0, 0.00001);
+    }
+
+    @Test
+    void evaluateConfiguration2(){
+        FeatureSystem featureSystem = getExampleFeatureSystem();
+        var result = featureSystem.evaluateFeatureConfiguration(getExampleFeatureConfiguration2());
+        assertEquals(result.size(), 3);
+        assertEquals(result.get(new Property("property_1", null, false)), 1.4, 0.00001);
+        assertEquals(result.get(new Property("property2", null, false)), -0.945, 0.00001);
+        assertEquals(result.get(new Property("property3", null, false)), 1.5342245214, 0.00001);
+    }
+}
