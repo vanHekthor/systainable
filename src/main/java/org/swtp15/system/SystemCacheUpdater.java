e378504d6aa8bac3136c28a5e5634ab0c6671592
Paul Hans Glass <pg98kona@studserv.uni-leipzig.de>
2021-01-19 16:17:48 +0100

    #60 Add outside system location for loading systems

--- src/main/java/org/swtp15/system/SystemCacheUpdater.java
+++ src/main/java/org/swtp15/system/SystemCacheUpdater.java
@@ -31,10 +31,13 @@ public class SystemCacheUpdater {
 
     /**
      * Initializes the {@link SystemCache}.
+     *
+     * This method scans both the internal used path of the models as well as `$PWD/modelFiles` to search for valid
+     * models.
      */
     public void initialize() {
         try {
-            readSystemsFromDirectory(this.systemCache.getPathToModels());
+            readSystemsFromDirectory(this.systemCache.getPathToModels(), new File("modelFiles").getCanonicalPath());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -42,52 +45,57 @@ public class SystemCacheUpdater {
     }
 
     /**
-     * Reads all {@link FeatureSystem} by reading all dimacs from one and all csv files from another directory and
-     * comparing their filenames.
+     * Reads systems from files
      *
-     * @param path Directory to find dimacs and csv files.
+     * Reads by going through all subdirectories of the provided paths and checks whether
+     * they contain both a valid dimacs, csv and optionally a xml, compiling a {@link FeatureSystem} for each found.
      *
-     * @throws IllegalArgumentException invalid path or path does not contains subdirectories for dimacs and csv.
+     * @param paths All directories to scan subdirectories from
+     *
+     * @throws IllegalArgumentException No subdirectories found or invalid model files
      */
-    public void readSystemsFromDirectory(String path) throws IllegalArgumentException {
-        File modelsDirectory = new File(path);
-        if (!(modelsDirectory.exists() && modelsDirectory.isDirectory())) {
+    public void readSystemsFromDirectory(String... paths) throws IllegalArgumentException {
+        Set<File> foundDirestories = Arrays.stream(paths).map(File::new).filter(File::isDirectory).collect(
+                Collectors.toSet());
+        if (foundDirestories.size() == 0) {
             throw SystemExceptions.INVALID_DIRECTORY_PATH;
         }
-        Set<Map<String, File>> readSystems = Arrays.stream(
-                Objects.requireNonNull(modelsDirectory.listFiles())).parallel().filter(File::isDirectory)
-                .map(directory -> {
-                    File[] notNullFiles = Objects.requireNonNull(directory.listFiles());
-                    List<File> dimacsFiles = Arrays.stream(notNullFiles).parallel()
-                            .filter(f -> f.getName().endsWith(".dimacs")).collect(Collectors.toList());
-                    List<File> xmlFiles = Arrays.stream(notNullFiles).parallel()
-                            .filter(f -> f.getName().endsWith(".xml")).collect(Collectors.toList());
-                    List<File> csvFiles = Arrays.stream(notNullFiles).parallel()
-                            .filter(f -> f.getName().endsWith(".csv")).collect(Collectors.toList());
-
-                    Map<String, File> fileTypeMap = new HashMap<>();
-                    fileTypeMap.put("name", directory);
-                    fileTypeMap.put("dimacs", dimacsFiles.get(0));
-                    fileTypeMap.put("xml", xmlFiles.size() == 0 ? null : xmlFiles.get(0));
-                    fileTypeMap.put("csv", csvFiles.get(0));
-
-                    return dimacsFiles.size() == 1 & csvFiles.size() == 1 & xmlFiles.size() <= 1 ?
-                           fileTypeMap : null;
-                }).filter(Objects::nonNull).collect(Collectors.toSet());
-
         Map<String, FeatureSystem> systemMap = new HashMap<>();
 
+        for (File dir : foundDirestories) {
+            Set<Map<String, File>> readSystems = Arrays.stream(
+                    Objects.requireNonNull(dir.listFiles())).parallel().filter(File::isDirectory)
+                    .map(directory -> {
+                        File[] notNullFiles = Objects.requireNonNull(directory.listFiles());
+                        List<File> dimacsFiles = Arrays.stream(notNullFiles).parallel()
+                                .filter(f -> f.getName().endsWith(".dimacs")).collect(Collectors.toList());
+                        List<File> xmlFiles = Arrays.stream(notNullFiles).parallel()
+                                .filter(f -> f.getName().endsWith(".xml")).collect(Collectors.toList());
+                        List<File> csvFiles = Arrays.stream(notNullFiles).parallel()
+                                .filter(f -> f.getName().endsWith(".csv")).collect(Collectors.toList());
 
-        for (Map<String, File> systemFileNames : readSystems) {
-            File xmlEntry = systemFileNames.get("xml");
-            FeatureModel featureModel = FeatureModelParser.parseModel(systemFileNames.get("dimacs").getPath(),
-                                                                      xmlEntry == null ? null : xmlEntry.getPath());
-            PerformanceInfluenceModel pIModel = PerformanceModelParser
-                    .parseModel(systemFileNames.get("csv").getPath(), featureModel.getFeatures());
+                        Map<String, File> fileTypeMap = new HashMap<>();
+                        fileTypeMap.put("name", directory);
+                        fileTypeMap.put("dimacs", dimacsFiles.get(0));
+                        fileTypeMap.put("xml", xmlFiles.size() == 0 ? null : xmlFiles.get(0));
+                        fileTypeMap.put("csv", csvFiles.get(0));
 
-            FeatureSystem system = new FeatureSystem(systemFileNames.get("name").getName(), featureModel, pIModel);
+                        return dimacsFiles.size() == 1 & csvFiles.size() == 1 & xmlFiles.size() <= 1 ?
+                               fileTypeMap : null;
+                    }).filter(Objects::nonNull).collect(Collectors.toSet());
 
-            systemMap.put(system.getName(), system);
+
+            for (Map<String, File> systemFileNames : readSystems) {
+                File xmlEntry = systemFileNames.get("xml");
+                FeatureModel featureModel = FeatureModelParser.parseModel(systemFileNames.get("dimacs").getPath(),
+                                                                          xmlEntry == null ? null : xmlEntry.getPath());
+                PerformanceInfluenceModel pIModel = PerformanceModelParser
+                        .parseModel(systemFileNames.get("csv").getPath(), featureModel.getFeatures());
+
+                FeatureSystem system = new FeatureSystem(systemFileNames.get("name").getName(), featureModel, pIModel);
+
+                systemMap.put(system.getName(), system);
+            }
         }
 
         this.systemCache.setCurrentlyKnownSystems(systemMap);
