1663d4f974a61cb42e1167766ef8ce6b328fbf1b
Paul Hans Glass <pg98kona@studserv.uni-leipzig.de>
2021-01-25 18:45:59 +0100

    #101 Multithreaded system location

--- src/main/java/org/swtp15/system/SystemCacheUpdater.java
+++ src/main/java/org/swtp15/system/SystemCacheUpdater.java
@@ -61,57 +61,71 @@ public class SystemCacheUpdater {
      * @throws IllegalArgumentException No subdirectories found or invalid model files
      */
     public void readSystemsFromDirectory(String... paths) throws IllegalArgumentException {
-        Set<File> foundDirestories = Arrays.stream(paths).map(File::new).filter(File::isDirectory).collect(
-                Collectors.toSet());
+        List<File> foundDirestories = Arrays.stream(paths).map(File::new).filter(File::isDirectory).collect(
+                Collectors.toList());
         Map<String, FeatureSystem> systemMap = new HashMap<>();
 
-        for (File dir : foundDirestories) {
-            Set<Map<String, File>> readSystems = Arrays.stream(
-                    Objects.requireNonNull(dir.listFiles())).parallel().filter(File::isDirectory)
-                    .map(directory -> {
-                        File[] notNullFiles = Objects.requireNonNull(directory.listFiles());
-                        List<File> dimacsFiles = Arrays.stream(notNullFiles).parallel()
-                                .filter(f -> f.getName().endsWith(".dimacs")).collect(Collectors.toList());
-                        List<File> xmlFiles = Arrays.stream(notNullFiles).parallel()
-                                .filter(f -> f.getName().endsWith(".xml")).collect(Collectors.toList());
-                        List<File> csvFiles = Arrays.stream(notNullFiles).parallel()
-                                .filter(f -> f.getName().endsWith(".csv")).collect(Collectors.toList());
+        Thread[] locatorThreads = new Thread[foundDirestories.size()];
 
-                        Map<String, File> fileTypeMap = new HashMap<>();
-                        fileTypeMap.put("name", directory);
-                        fileTypeMap.put("dimacs", dimacsFiles.get(0));
-                        fileTypeMap.put("xml", xmlFiles.size() == 0 ? null : xmlFiles.get(0));
-                        fileTypeMap.put("csv", csvFiles.get(0));
+        for (int i = 0; i < foundDirestories.size(); i++) {
+            File dir = foundDirestories.get(i);
+            locatorThreads[i] = new Thread(() -> {
+                Set<Map<String, File>> readSystems = Arrays.stream(
+                        Objects.requireNonNull(dir.listFiles())).parallel()
+                        .filter(File::isDirectory).map(directory -> {
+                            File[] notNullFiles = Objects.requireNonNull(directory.listFiles());
+                            List<File> dimacsFiles = Arrays.stream(notNullFiles)
+                                    .filter(f -> f.getName().endsWith(".dimacs")).collect(Collectors.toList());
+                            List<File> xmlFiles = Arrays.stream(notNullFiles).filter(f -> f.getName().endsWith(".xml"))
+                                    .collect(Collectors.toList());
+                            List<File> csvFiles = Arrays.stream(notNullFiles).filter(f -> f.getName().endsWith(".csv"))
+                                    .collect(Collectors.toList());
 
-                        return dimacsFiles.size() == 1 & csvFiles.size() == 1 & xmlFiles.size() <= 1 ?
-                               fileTypeMap : null;
-                    }).filter(Objects::nonNull).collect(Collectors.toSet());
+                            Map<String, File> fileTypeMap = new HashMap<>();
+                            fileTypeMap.put("name", directory);
+                            fileTypeMap.put("dimacs", dimacsFiles.get(0));
+                            fileTypeMap.put("xml", xmlFiles.size() == 0 ? null : xmlFiles.get(0));
+                            fileTypeMap.put("csv", csvFiles.get(0));
+
+                            return dimacsFiles.size() == 1 & csvFiles.size() == 1 & xmlFiles.size() <= 1 ?
+                                   fileTypeMap : null;
+                        }).filter(Objects::nonNull).collect(Collectors.toSet());
 
 
-            for (Map<String, File> systemFileNames : readSystems) {
-                File xmlEntry = systemFileNames.get("xml");
-                try {
-                    FeatureModel featureModel = FeatureModelParser.parseModel(systemFileNames.get("dimacs").getPath(),
-                                                                              xmlEntry == null ? null :
-                                                                              xmlEntry.getPath(), false);
-                    PerformanceInfluenceModel pIModel = PerformanceModelParser
-                            .parseModel(systemFileNames.get("csv").getPath(), featureModel.getFeatures(), false);
+                for (Map<String, File> systemFileNames : readSystems) {
+                    File xmlEntry = systemFileNames.get("xml");
+                    try {
+                        FeatureModel featureModel = FeatureModelParser
+                                .parseModel(systemFileNames.get("dimacs").getPath(),
+                                            xmlEntry == null ? null :
+                                            xmlEntry.getPath(), false);
+                        PerformanceInfluenceModel pIModel = PerformanceModelParser
+                                .parseModel(systemFileNames.get("csv").getPath(), featureModel.getFeatures(), false);
 
-                    FeatureSystem system = new FeatureSystem(systemFileNames.get("name").getName(), featureModel,
-                                                             pIModel);
+                        FeatureSystem system = new FeatureSystem(systemFileNames.get("name").getName(), featureModel,
+                                                                 pIModel);
 
-                    systemMap.put(system.getName(), system);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+                        systemMap.put(system.getName(), system);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
+            });
         }
+
+        Arrays.stream(locatorThreads).forEach(Thread::run);
+        Arrays.stream(locatorThreads).forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 
         if (permanentSystems != null) {
             systemMap.putAll(permanentSystems.parallelStream().collect(Collectors.toMap(FeatureSystem::getName,
                                                                                         system -> system)));
         }
-
 
         this.systemCache.setCurrentlyKnownSystems(systemMap);
     }
