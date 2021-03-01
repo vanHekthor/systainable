b60341c31be662175df97465390c3e7b9091ba0f
Quoc Duong Bui <bui.qcd@gmail.com>
2021-03-01 23:21:57 +0100

    Changed functions of colorPalette.js to have amount of colors needed as parameter instead of an array.
    Changed parameter in function call to colorArrayRadar(configNames.length)  in ChartDataBuilder.js, because that is the required number of colors for the radar chart.

--- src/main/webapp/app/js_modules/visualization/colorPalette.js
+++ src/main/webapp/app/js_modules/visualization/colorPalette.js
@@ -1,21 +1,18 @@
 import chroma from 'chroma-js';
 
 export default {
-  colorArrayChart: function colorArrayChart(propLength) {
-    let count = propLength.length;
-
-    return chroma.scale(['#0288D1', '#D32F2F']).mode('lch').colors(count);
+  colorArrayChart: function colorArrayChart(colorAmount) {
+    return chroma.scale(['#0288D1', '#D32F2F']).mode('lch').colors(colorAmount);
   },
 
-  colorArrayRadar: function colorArrayRadar(propLength) {
-    let count = propLength.length;
+  colorArrayRadar: function colorArrayRadar(colorAmount) {
     return {
-      backgroundColor: chroma.scale(['rgba(0, 0, 0, 0)']).mode('lch').colors(count),
-      borderColor: chroma.scale(['#FF6600', '#00FF3C', '#0288D1']).mode('lch').colors(count),
-      pointBackgroundColor: chroma.scale(['#FF6600', '#00FF3C', '#0288D1']).mode('lch').colors(count),
-      pointBorderColor: chroma.scale(['#fff']).mode('lch').colors(count),
-      pointHoverBackgroundColor: chroma.scale(['#FF6600', '#00FF3C', '#0288D1']).mode('lch').colors(count),
-      pointHoverBorderColor: chroma.scale(['#FF6384', '#00FF3C']).mode('lch').colors(count),
+      backgroundColor: chroma.scale(['rgba(0, 0, 0, 0)']).mode('lch').colors(colorAmount),
+      borderColor: chroma.scale(['#FF6600', '#00FF3C', '#0288D1']).mode('lch').colors(colorAmount),
+      pointBackgroundColor: chroma.scale(['#FF6600', '#00FF3C', '#0288D1']).mode('lch').colors(colorAmount),
+      pointBorderColor: chroma.scale(['#fff']).mode('lch').colors(colorAmount),
+      pointHoverBackgroundColor: chroma.scale(['#FF6600', '#00FF3C', '#0288D1']).mode('lch').colors(colorAmount),
+      pointHoverBorderColor: chroma.scale(['#FF6384', '#00FF3C']).mode('lch').colors(colorAmount),
     };
   },
 };
