89c8bae74cdc5068a13f39e5d6ab9950a037df0a
Quoc Duong Bui <qb19zexe@studserv.uni-leipzig.de>
2021-03-18 21:30:33 +0100

    Removed outdated lines for config cards in ConfigArea.vue.
    Minor code style changes in ConfigArea.vue.
    
    Minor component style changes for ConfigCard.vue. Darker border and no shadow in config area. Lighter border and shadow in dialogs.

--- src/main/webapp/app/components/ConfigArea.vue
+++ src/main/webapp/app/components/ConfigArea.vue
@@ -37,83 +37,9 @@
             </template>
             <b-collapse id="collapse-configs" v-model="visible">
                 <div v-if="selectedViewOption==='simple'"
-                     class="panel-content p-d-flex"
+                     class="panel-content d-flex"
                      style="overflow-x: auto;">
-                    <div class="p-d-flex">
-<!--                        <div v-for="(config, index) in configurations" :key="index"
-                             class="config-card p-shadow-1"
-                             style="width: 18rem">
-                            <div class="config-card-header p-p-1">
-                                <div class="p-d-flex p-ai-center p-jc-between">
-                                    <h6 class="p-my-0 py-1 p-ml-3" style="max-width: 12rem; overflow-x: auto">
-                                        {{config.name}}
-                                    </h6>
-                                    <div>
-                                        <b-button class="p-1 no-outline" variant="link"
-                                                  @click.stop="toggle($event, index)">
-                                            <font-awesome-icon icon="plus" :style="{ color: '#6c757d' }" fixed-width/>
-                                        </b-button>
-                                        <b-dropdown class="p-0 no-outline" toggle-class="p-1 no-outline" variant="link" boundary="viewport" no-caret
-                                                    @toggle="hideFeatureList()">
-                                            <template #button-content>
-                                                <font-awesome-icon icon="ellipsis-v" :style="{ color: '#6c757d' }" fixed-width/>
-                                            </template>
-                                            <b-dropdown-item-button @click="$emit('duplicate-config',index)">
-                                                <font-awesome-icon icon="copy" class="mr-1" :style="{ color: '#6c757d' }" fixed-width/>
-                                                duplicate
-                                            </b-dropdown-item-button>
-                                            <b-dropdown-item-button v-b-modal="'modal-' + index" @click="renamedConfigString=config.name">
-                                                <font-awesome-icon icon="edit" class="mr-1" :style="{ color: '#6c757d' }" fixed-width/>
-                                                rename
-                                            </b-dropdown-item-button>
-                                            <b-dropdown-item-button @click="$emit('del-config', index)">
-                                                <font-awesome-icon icon="trash" class="mr-1" :style="{ color: '#6c757d' }" fixed-width/>
-                                                delete
-                                            </b-dropdown-item-button>
-                                            <b-dropdown-divider></b-dropdown-divider>
-                                            <b-dropdown-item-button @click="$emit('click-optimize', config.name, '')">
-                                                <font-awesome-icon icon="compass" class="mr-1" :style="{ color: '#6c757d' }" fixed-width/>
-                                                optimize
-                                            </b-dropdown-item-button>
-                                        </b-dropdown>
-                                        <b-modal :id="'modal-' + index" centered :title="'Rename configuration: ' + config.name"
-                                                 @ok="$emit('update-config-name', index, renamedConfigString)">
-                                            <b-form-input type="text" :value="config.name" v-model="renamedConfigString" maxlength="24"></b-form-input>
-                                        </b-modal>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="config-card-content p-p-1"
-                                 style="min-height: 6rem; max-height: 16rem; overflow-y: auto">
-                                <div class="badges p-d-flex p-flex-wrap p-jc-center">
-                                    <template v-for="(featureName) in systemFeatures.binaryFeatures">
-                                        <Chip v-if="featureName !== 'name' && config[featureName]"
-                                              class="binary-chip p-m-1" :label="featureName" removable
-                                              @remove="$emit('update-feature', index, featureName, false)"/>
-                                    </template>
-                                    <template v-for="(featureName) in Object.keys(systemFeatures.numericFeatures)">
-                                        <Chip :id="'numeric-chip' + index + featureName"
-                                              class="numeric-chip p-m-1" :label="featureName + ': ' + config[featureName]"/>
-                                        <b-popover
-                                            :target="'numeric-chip' + index + featureName"
-                                            :title="featureName"
-                                            triggers="hover"
-                                            boundary="viewport"
-                                            placement="bottom">
-                                            <CustomSpinButton
-                                                :value="config[featureName]"
-                                                :value.sync="config[featureName]"
-                                                :step-function="systemFeatures.numericFeatures[featureName].stepFunction"
-                                                :min="systemFeatures.numericFeatures[featureName].min"
-                                                :max="systemFeatures.numericFeatures[featureName].max"
-                                            />
-                                        </b-popover>
-                                    </template>
-                                </div>
-                            </div>
-                            <div class="config-card-footer p-d-flex p-jc-center">
-                            </div>
-                        </div>-->
+                    <div class="d-flex">
                         <template v-for="(config, index) in configurations">
                             <ConfigCard
                                 :config="config"
@@ -350,64 +276,49 @@ export default {
 </script>
 
 <style scoped>
-.panel-header {
-    margin-top: -0.5rem;
-    margin-bottom: -0.5rem;
-}
 
-.panel-content {
-    margin: -1rem -1rem 0 -1rem;
-    padding-bottom: 0.5em;
-}
+    .panel-header {
+        margin-top: -0.5rem;
+        margin-bottom: -0.5rem;
+    }
 
-.panel-footer {
-    margin: 0 -0.5rem -0.5rem -0.5rem;
-}
+    .panel-content {
+        margin: -1rem -1rem 0 -1rem;
+        padding-bottom: 0.5em;
+    }
 
-.config-card-header{
-    background-color: #f8f9fa;
-    border-bottom: 1px solid #e3e3e3;
-}
+    .panel-footer {
+        margin: 0 -0.5rem -0.5rem -0.5rem;
+    }
 
-.binary-chip {
-    background: #2196F3;
-    color: #fff;
-}
-
-.numeric-chip {
-    background: #0064C1;
-    color: #fff;
-}
-
-.no-outline:focus,.no-outline:active {
-    outline: none !important;
-    box-shadow: none !important;
-}
 </style>
+
 <style>
-.dropdown:active .dropdown:focus .dropdown-menu .dropdown-item {
-    outline: none;
-    box-shadow: none;
-}
 
-.p-overlaypanel {
-    box-shadow: none !important;
-    border: 1px rgba(0, 0, 0, 0.15) !important;
-    border-style: solid !important;
-    border-radius: 1px;
-    --overlayArrowLeft: none !important;
-    margin: 0.125rem;
-}
+    .dropdown:active .dropdown:focus .dropdown-menu .dropdown-item {
+        outline: none;
+        box-shadow: none;
+    }
 
-.p-overlaypanel-content {
-    padding: 0.5rem 0 !important;
-}
+    .p-overlaypanel {
+        box-shadow: none !important;
+        border: 1px rgba(0, 0, 0, 0.15) !important;
+        border-style: solid !important;
+        border-radius: 1px;
+        --overlayArrowLeft: none !important;
+        margin: 0.125rem;
+    }
 
-.table tr, .table td {
-    padding: 0.25rem 1.5rem !important;
-}
+    .p-overlaypanel-content {
+        padding: 0.5rem 0 !important;
+    }
 
-.hidden-header {
-    display: none;
-}
+    .hidden-header {
+        display: none;
+    }
+
+    .table tr, .table td {
+        padding: 0.25rem 1.5rem !important;
+    }
+
 </style>
